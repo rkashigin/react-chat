825e2ed62dc6292caea3d2a38ff20970fc5e8aab
Roman Kashigin <r.kashigin@yandex.ru>
2021-02-20 21:48:37 +0300

    Schema and model for users created

--- chat-backend/src/index.ts
+++ chat-backend/src/index.ts
@@ -1,5 +1,13 @@
-const func = (a: number, b: number) => {
-  return a + b;
-};
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
 
-console.log(func(1, 2));
+mongoose.connect("mongodb://localhost:27017/chat", { useNewUrlParser: true });
+
+app.get("/", function (_: any, res: any) {
+  res.send("Hello World!");
+});
+
+app.listen(3000, function () {
+  console.log("Example app listening on port 3000!");
+});
---
+++ chat-backend/src/schemas/User.ts
@@ -0,0 +1,20 @@
+import { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: String,
+    avatar: String,
+    fullName: String,
+    password: String,
+    confirmed: Boolean,
+    confirm_hash: String,
+    last_seen: Date,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const User = mongoose.model("User", UserSchema);
+
+export default User;
